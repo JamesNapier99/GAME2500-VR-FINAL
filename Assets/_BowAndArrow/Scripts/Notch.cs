0d7122e00571a7cc5b8843539de249d44ac43c73
James Napier <72824555+JamesNapier99@users.noreply.github.com>
2020-12-01 12:59:32 -0500

    First Stable Android Buil
    -Added XR Bow+Arrow functionality
    -Added simplemodularhuman setup
    -Stable build .apk (testingWpeople)
    
    ~No matching guide image

---
+++ Assets/_BowAndArrow/Scripts/Notch.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Notch : XRSocketInteractor
+{
+    private Puller puller = null;
+    private Arrow currentArrow = null;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        puller = GetComponent<Puller>();
+    }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        puller.onSelectExit.AddListener(TryToReleaseArrow);
+    }
+
+    protected override void OnDisable()
+    {
+        base.OnDisable();
+        puller.onSelectExit.RemoveListener(TryToReleaseArrow);
+    }
+
+    protected override void OnSelectEnter(XRBaseInteractable interactable)
+    {
+        base.OnSelectEnter(interactable);
+        StoreArrow(interactable);
+    }
+
+    private void StoreArrow(XRBaseInteractable interactable)
+    {
+        if (interactable is Arrow arrow)
+            currentArrow = arrow;
+    }
+
+    private void TryToReleaseArrow(XRBaseInteractor interactor)
+    {
+        if (currentArrow)
+        {
+            ForceDeselect();
+            ReleaseArrow(); 
+        }
+    }
+
+    private void ForceDeselect()
+    {
+        base.OnSelectExit(currentArrow);
+        currentArrow.OnSelectExit(this);
+    }
+
+    private void ReleaseArrow()
+    {
+        currentArrow.Release(puller.PullAmount);
+        currentArrow = null;
+    }
+
+    public override XRBaseInteractable.MovementType? selectedInteractableMovementTypeOverride
+    {
+        get { return XRBaseInteractable.MovementType.Instantaneous; } 
+    }
+}
