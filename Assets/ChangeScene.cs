bf5c8a50d605e7c469870863d873073de361a00b
James Napier <72824555+JamesNapier99@users.noreply.github.com>
2020-12-07 22:59:02 -0500

    Major Updates

--- Assets/ChangeScene.cs
+++ Assets/ChangeScene.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VRInputModule : MonoBehaviour
+public class ChangeScene : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
